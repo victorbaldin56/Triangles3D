8715332574fad0fd925f9809af91e835ca76d604
Victor Baldin <victor.baldin.2005@gmail.com>
2024-10-14 23:01:19 +0300

    add some tests

--- tests/geometry_tests.cc
+++ tests/geometry_tests.cc
@@ -2,6 +2,14 @@
 
 #include "vector3d.hh"
 
+using namespace geometry;
+
+TEST(Vector3D, add) {
+  Vector3D<float> v1{2, 1, 3};
+  Vector3D<float> v2{1, 4.1, 5};
+  ASSERT_TRUE((v1 + v2).isClose({3, 5.1, 8}));
+}
+
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
