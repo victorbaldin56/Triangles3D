bda9cf26b3f7c9b2c4c6e405934d8589d2321565
Victor Baldin <victor.baldin.2005@gmail.com>
2024-10-24 13:11:48 +0300

    add triangles input

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,15 @@
+#include <vector>
+
+#include "triangle3d.hh"
+
 int main() {
+  std::size_t count = 0;
+  std::cin >> count;
+
+  std::vector<geometry::Triangle3D<float>> triangles(count);
+  for (auto& t : triangles) {
+    std::cin >> t;
+  }
+
   return 0;
 }
