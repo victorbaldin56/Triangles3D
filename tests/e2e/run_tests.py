6876f0385713e9936be8f25c14216194172d1074
Victor Baldin <victor.baldin.2005@gmail.com>
2025-03-13 00:23:14 +0300

    refactor e2e tests

--- tests/e2e/run_tests.py
+++ tests/e2e/run_tests.py
@@ -1,46 +1,42 @@
 import os
 import subprocess
+import glob
 
 CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
 PATH_TO_EXECUTABLE = CURRENT_PATH + '/../../build/driver/triangles'
-PATH_TO_INPUT = CURRENT_PATH + '/func/input/'
-PATH_TO_ANSWERS = CURRENT_PATH + '/func/ans/'
+PATH_TO_INPUT = [CURRENT_PATH + '/func/input/', CURRENT_PATH + '/bench/input']
 
-def inputFilePath(test_num):
-  return PATH_TO_INPUT + f'test_{test_num}.in'
-
-def ansFilePath(test_num):
-  return PATH_TO_ANSWERS + f'ans_{test_num}.out'
+def ansFilePath(input_path):
+  return os.path.join((os.path.dirname(input_path)), os.path.pardir) + "/ans/" + (os.path.basename(input_path).replace('test_', 'ans_').replace('.in', '.out'))
 
 def getAns(ans_file):
   with open(ans_file, 'r') as af:
     return af.read()
 
-def test(test_num):
+def test():
   fail = False
 
-  for test_num in range(10):
-    input_path = inputFilePath(test_num)
-    ans_path = ansFilePath(test_num)
-    with open(input_path, 'r') as input_file:
-      process = subprocess.run(
-        [PATH_TO_EXECUTABLE],
-        stdin=input_file, text=True, capture_output=True
-      )
-      if process.returncode != 0:
-        raise RuntimeError(f'Driver failed: {process.stderr}')
-      output = process.stdout
-      ref_output = getAns(ans_path)
-      if (output != ref_output):
-        print(f"Test {test_num} failed\n"
-              f"Expected: {ref_output}\n"
-              f"Actual:   {output}\n")
-        fail = True
-      else:
-        print(f"Test {test_num} passed")
+  for i in range(len(PATH_TO_INPUT)):
+    for input_path in glob.glob(os.path.join(PATH_TO_INPUT[i], '*')):
+      ans_path = ansFilePath(input_path)
+      with open(input_path, 'r') as input_file:
+        process = subprocess.run(
+          [PATH_TO_EXECUTABLE],
+          stdin=input_file, text=True, capture_output=True
+        )
+        if process.returncode != 0:
+          raise RuntimeError(f'Driver failed on test {input_path}: {process.stderr}')
+        output = process.stdout
+        ref_output = getAns(ans_path)
+        if (output != ref_output):
+          print(f"Test {input_path} failed\n"
+                f"Expected: {ref_output}\n"
+                f"Actual:   {output}\n")
+          fail = True
+        else:
+          print(f"Test {ans_path} passed")
 
   if fail:
     raise RuntimeError("End-to-end test failed\n")
 
-for i in range(10):
-  test(i)
+test()
