b9992d7aea9e42fbae191cd07e668affad64b981
Victor Baldin <victor.baldin.2005@gmail.com>
2025-04-10 12:27:27 +0300

    add Texture handle

--- glhpp/include/glhpp/texture.hh
+++ glhpp/include/glhpp/texture.hh
@@ -2,12 +2,21 @@
 
 #include <memory>
 
+#include "light.hh"
 #include "shader.hh"
 
 namespace glhpp {
 
 class Texture final {
+  static void deleteHandle(GLuint p) noexcept { glDeleteTextures(1, &p); }
+  using Handle = std::unique_ptr<void, detail::ObjectDeleter<deleteHandle>>;
 
+ public:
+  Texture(const Light& light, unsigned vcount,
+          const std::vector<Shader> shaders);
+
+ private:
+  Handle handle_;
 };
 
 }
