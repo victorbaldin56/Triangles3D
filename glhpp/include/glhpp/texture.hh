3834dddf5bbf7056b96c7cd033c1d436f2c13fe3
Victor Baldin <victor.baldin.2005@gmail.com>
2025-04-10 17:44:06 +0300

    add renderer rountines

--- glhpp/include/glhpp/texture.hh
+++ glhpp/include/glhpp/texture.hh
@@ -26,15 +26,52 @@ class Texture final {
         program_(shaders),
         light_dir_(light.dir),
         width_(light.width),
-        height_(light.height) {
-    drawShadows(light, vcount, figure_type);
+        height_(light.height),
+        vertex_count_(vcount) {
+    drawShadows(light, figure_type);
   }
 
   Texture(Texture&& rhs) noexcept = default;
   Texture& operator=(Texture&& rhs) noexcept = default;
 
+  auto setUniformShadowMap(GLuint program_id) const {
+    GLHPP_DETAIL_ERROR_HANDLER(glActiveTexture, GL_TEXTURE0);
+    GLHPP_DETAIL_ERROR_HANDLER(glBindTexture, GL_TEXTURE_2D, handle_.get());
+    GLHPP_DETAIL_ERROR_HANDLER(
+        glUniform1i,
+        GLHPP_DETAIL_ERROR_HANDLER(glGetUniformLocation, program_id,
+                                   "shadow_map"),
+        0);
+  }
+
+  auto setUniformLightDirection(GLuint program_id) const {
+    auto light_dir_normalized = glm::normalize(light_dir_);
+    GLHPP_DETAIL_ERROR_HANDLER(
+        glUniform3f,
+        GLHPP_DETAIL_ERROR_HANDLER(glGetUniformLocation, program_id,
+                                   "light_dir"),
+        light_dir_normalized.x, light_dir_normalized.y, light_dir_normalized.z);
+  }
+
+  auto setUniformDepthBiasMvp(GLuint program_id) const {
+    // clang-format off
+    glm::mat4 bias_matrix(0.5, 0.0, 0.0, 0.0,
+                          0.0, 0.5, 0.0, 0.0,
+                          0.0, 0.0, 0.5, 0.0,
+                          0.5, 0.5, 0.5, 1.0);
+    // clang-format on
+    glm::mat4 depth_bias_mvp = bias_matrix * depth_mvp_;
+    GLHPP_DETAIL_ERROR_HANDLER(
+        glUniformMatrix4fv,
+        GLHPP_DETAIL_ERROR_HANDLER(glGetUniformLocation, program_id,
+                                   "depth_bias_MVP"),
+        1, GL_FALSE, &depth_bias_mvp[0][0]);
+  }
+
+  auto vertexCount() const noexcept { return vertex_count_; }
+
  private:
-  void drawShadows(const Light& light, std::size_t vcount, int figure_type) {
+  void drawShadows(const Light& light, int figure_type) {
     bind();
     Framebuffer fb(genFramebuffer());
     GLHPP_DETAIL_ERROR_HANDLER(glBindFramebuffer, GL_FRAMEBUFFER, fb.get());
@@ -44,12 +81,12 @@ class Texture final {
                                GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D,
                                handle_.get(), 0);
 
-    setUniformMvp(program_.id(), light);
+    setUniformDepthMvp(light);
 
     GLHPP_DETAIL_ERROR_HANDLER(glViewport, 0, 0, width_, height_);
     GLHPP_DETAIL_ERROR_HANDLER(glClear,
                                GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    GLHPP_DETAIL_ERROR_HANDLER(glDrawArrays, figure_type, 0, vcount);
+    GLHPP_DETAIL_ERROR_HANDLER(glDrawArrays, figure_type, 0, vertex_count_);
     GLHPP_DETAIL_ERROR_HANDLER(glBindFramebuffer, GL_FRAMEBUFFER, 0);
   }
 
@@ -68,13 +105,16 @@ class Texture final {
                                GL_TEXTURE_WRAP_T, GL_REPEAT);
   }
 
-  void setUniformMvp(GLuint program_id, const Light& light) {
+  void setUniformDepthMvp(const Light& light) {
     auto depth_view_matrix = glm::lookAt(light.pos, light.dir, light.up);
     glm::mat4 depth_model_matrix(1.f);
-    mvp_ = light.projection_matrix * depth_view_matrix * depth_model_matrix;
-    GLHPP_DETAIL_ERROR_HANDLER(glUniformMatrix4fv,
-                               glGetUniformLocation(program_id, "mvp"), 1,
-                               GL_FALSE, &mvp_[0][0]);
+    depth_mvp_ =
+        light.projection_matrix * depth_view_matrix * depth_model_matrix;
+    GLHPP_DETAIL_ERROR_HANDLER(
+        glUniformMatrix4fv,
+        GLHPP_DETAIL_ERROR_HANDLER(glGetUniformLocation, program_.id(),
+                                   "depth_mvp"),
+        1, GL_FALSE, &depth_mvp_[0][0]);
   }
 
   static GLuint genTexture() {
@@ -92,9 +132,10 @@ class Texture final {
  private:
   Handle handle_;
   Program program_;
-  unsigned width_ = 0;
-  unsigned height_ = 0;
-  glm::mat4 mvp_;
+  unsigned width_;
+  unsigned height_;
+  std::size_t vertex_count_;
+  glm::mat4 depth_mvp_;
   glm::vec3 light_dir_;
 };
 
