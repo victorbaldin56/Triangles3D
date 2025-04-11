8ffb5b3b87d02f26a36380ce5c31d9a0d8bac001
Victor Baldin <victor.baldin.2005@gmail.com>
2025-04-11 10:52:09 +0300

    add Camera methods

--- triangles_gl/include/triangles_gl/camera.hh
+++ triangles_gl/include/triangles_gl/camera.hh
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "glm/glm.hpp"
+#include "glm/gtc/quaternion.hpp"
 
 namespace triangles_gl {
 
 class Camera final {
  public:
-  Camera(glm::vec3 pos = {0, 0, 25}, glm::vec3 dir = {0, 0, -1},
-         glm::vec3 up = {0, 1, 0}, float fov = glm::radians(45.f),
+  Camera(const glm::vec3& pos = {0, 0, 25}, const glm::vec3& dir = {0, 0, -1},
+         const glm::vec3& up = {0, 1, 0}, float fov = glm::radians(45.f),
          float z_near_clip = 0.1f, float z_far_clip = 1000.f) noexcept
       : position_(pos),
         direction_(glm::normalize(dir)),
@@ -15,6 +16,26 @@ class Camera final {
         fov_(fov),
         z_near_clip_(z_near_clip),
         z_far_clip_(z_far_clip) {}
+
+  auto translate(const glm::vec3& translation) noexcept {
+    position_ += translation;
+  }
+
+  const auto& getDirection() const noexcept { return direction_; }
+  const auto& getUp() const noexcept { return up_; }
+  auto getSideways() const noexcept { return glm::cross(direction_, up_); }
+
+  auto rotate(const glm::quat& q) noexcept {
+    direction_ = direction_ * q;
+    up_ = up_ * q;
+  }
+
+  auto getVpMatrix(unsigned width, unsigned height) const noexcept {
+    auto view = glm::lookAt(position_, position_ + direction_, up_);
+    auto proj = glm::perspective(fov_, static_cast<float>(width) / height,
+                                 z_near_clip_, z_far_clip_);
+    return proj * view;
+  }
 
  private:
   glm::vec3 position_;
