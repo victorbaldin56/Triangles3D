609d52fc3e02b340db52878b31e8e46fb75d4208
Victor Baldin <victor.baldin.2005@gmail.com>
2025-04-12 15:11:45 +0300

    add log

--- glhpp/include/glhpp/program.hh
+++ glhpp/include/glhpp/program.hh
@@ -5,7 +5,16 @@
 namespace glhpp {
 
 class Program final {
-  static void deleteHandle(GLuint p) noexcept { glDeleteProgram(p); }
+  static void deleteHandle(GLuint p) noexcept {
+    constexpr GLsizei kMaxCount = 2;
+    GLsizei count;
+    std::array<GLuint, kMaxCount> attached_shaders;
+    glGetAttachedShaders(p, kMaxCount, &count, attached_shaders.data());
+    for (int i = 0; i < count; ++i) {
+      glDetachShader(p, attached_shaders[i]);
+    }
+    glDeleteProgram(p);
+  }
   using Handle = std::unique_ptr<void, detail::ObjectDeleter<deleteHandle>>;
 
  public:
@@ -19,12 +28,34 @@ class Program final {
 
     GLint res;
     GLHPP_DETAIL_ERROR_HANDLER(glGetProgramiv, id, GL_LINK_STATUS, &res);
+    handleProgramiv(id, res);
   }
 
   Program(Program&& other) noexcept = default;
   Program& operator=(Program&& rhs) noexcept = default;
 
   auto id() const noexcept { return handle_.get(); }
+
+ private:
+  static void handleProgramiv(GLuint program_id, GLint res) {
+    if (res) {
+      return;
+    }
+
+    std::stringstream build_log;
+    build_log << "Error building program\n";
+    int log_length;
+    GLHPP_DETAIL_ERROR_HANDLER(glGetProgramiv, program_id, GL_INFO_LOG_LENGTH,
+                               &log_length);
+    if (log_length > 0) {
+      auto err_msg = std::make_unique<char[]>(log_length + 1);
+      GLHPP_DETAIL_ERROR_HANDLER(glGetProgramInfoLog, program_id, log_length,
+                                 nullptr, err_msg.get());
+      build_log << err_msg.get();
+    }
+
+    throw Error(build_log.str());
+  }
 
  private:
   Handle handle_;
